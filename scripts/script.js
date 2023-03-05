3207924015225e181fc0cc6eb2bf42d824947238
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-03-05 13:46:44 +0530

    Added resume

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let pdf = document.getElementsByClassName('pdfLink');
 
 let link = "/resume.pdf";
 
-for(let i = 0; i < pdf.length; i++){
+for (let i = 0; i < pdf.length; i++) {
     pdf[i].removeAttribute('href');
     pdf[i].setAttribute('href', link);
 }
