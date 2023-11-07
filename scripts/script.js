13fa34aaccfcf5ceb8ee7b7a2ebc25193dcd633f
septicalls <septicalls@proton.me>
2023-11-07 18:23:06 +0530

    removed broken workflow

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-let pdf = document.getElementsByClassName('pdfLink');
-
-let link = "/resume.pdf";
-
-for (let i = 0; i < pdf.length; i++) {
-    pdf[i].removeAttribute('href');
-    pdf[i].setAttribute('href', link);
-}
